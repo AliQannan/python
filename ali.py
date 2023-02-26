8c608fb5cf548fa8db3210dabde885b5d3aa79fe
ali <aliqannan2003f@gmail.com>
2023-02-27 00:07:08 +0200

    finish work

--- ali.py
+++ ali.py
@@ -1,15 +1,14 @@
 import random
-top_of_range = input("Type a number:")
-if top_of_range.isdigit():
-	top_of_range=int(top_of_range)
+def your_Number ():
+	top_of_range=input("Type a number : ")
 
-	if top_of_number <= 0 :
-		print("please Type a number larger than 0 next time.")
-
-
-		quit()
-else:
-	print("type a number next time.")
-	quit()
-
-random_number=random.randint(top_of_range)
+	if top_of_range.isdigit():
+		top_of_range=float(top_of_range)
+		print(top_of_range)
+		if top_of_range <=0:
+			print('type larger number next time')
+			
+	else:
+		print('type number please ? ')
+	return top_of_range , type(top_of_range) ,random.randint(0,top_of_range)
+print(your_Number())
